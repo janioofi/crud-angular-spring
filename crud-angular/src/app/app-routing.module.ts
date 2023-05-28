684c91139fd8a621d7a71800e14e1e5b51096d9c
Jânio Filho <filhojanio67@gmail.com>
2023-05-27 21:17:51 -0300

    Atualizando para scss e customizando as cores

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'courses' },
-    {
-        path: 'courses',
-        loadChildren: () =>
-            import('./courses/courses.module').then((m) => m.CoursesModule),
-    },
-];
+const routes: Routes = [];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
