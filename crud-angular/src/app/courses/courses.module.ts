dff465cdf049707323560dc9f2fad5e9897224e2
Jânio Filho <filhojanio67@gmail.com>
2023-05-30 18:13:59 -0300

    Tratamento de erros e MatDialog

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AppMaterialModule } from '../shared/app-material/app-material.module'
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
 	declarations: [CoursesComponent],
 	imports: [
 		CommonModule,
 		CoursesRoutingModule,
-		AppMaterialModule
+		AppMaterialModule,
+		SharedModule,
 	],
 })
 export class CoursesModule {}
--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,27 +1,40 @@
 import { CoursesService } from './../service/courses.service';
 import { Component, OnInit } from '@angular/core';
-import { Course } from '../model/course'
-import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+	selector: 'app-courses',
+	templateUrl: './courses.component.html',
+	styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent implements OnInit {
-
 	courses$: Observable<Course[]>;
-	displayedColumns = [ 'name', 'category'];
+	displayedColumns = ['name', 'category'];
 
 	//courseService :CoursesService;
 
-	constructor(private courseService :CoursesService){
+	constructor(
+		private courseService: CoursesService,
+		public dialog: MatDialog
+	) {
 		//this.courses = [];
 		//this.courseService = new CoursesService();
-		this.courses$ = this.courseService.list();
+		this.courses$ = this.courseService.list().pipe(
+			catchError((error) => {
+				this.onError("Erro ao carregar cursos");
+				return of([]);
+			})
+		);
+	}
+	ngOnInit(): void {
 	}
 
-	ngOnInit(): void {
-
+	onError(errorMsg: string) {
+		this.dialog.open(ErrorDialogComponent, {
+			data: errorMsg
+		});
 	}
 }
