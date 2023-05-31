593ece9fb73fe62f4f9d3cda03b6db27eebbf475
Jânio Filho <filhojanio67@gmail.com>
2023-05-31 18:57:12 -0300

    Crud spring criado

---
+++ crud-spring/src/test/java/com/jan1ooo/crudspring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan1ooo.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
