cec0243dc220c509071d0e6baf06d8fb693f55d1
Jânio Filho <filhojanio67@gmail.com>
2023-06-02 18:18:58 -0300

    Salvando os dados com HTTP POST + tratamento de erros

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,30 +1,36 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../service/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-  selector: 'app-course-form',
-  templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+	selector: 'app-course-form',
+	templateUrl: './course-form.component.html',
+	styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
 	form: FormGroup;
 
-	constructor(private formBuilder: FormBuilder){
+	constructor(
+		private formBuilder: FormBuilder,
+		private service: CoursesService,
+		private snackBar: MatSnackBar
+	) {
 		this.form = this.formBuilder.group({
 			name: [null],
-			category: [null]
-		})
+			category: [null],
+		});
 	}
 
 	onSubmit() {
-		
+		this.service.save(this.form.value).subscribe((result) => console.log(result),error => this.onError());
+			}
+
+	private onError(){
+		this.snackBar.open('Erro ao salvar curso', '', {duration: 5000});
 	}
 
-	onCancel(){
+	onCancel() {}
 
-	}
-
-	NgOnInit(): void{
-
-	}
+	NgOnInit(): void {}
 }
--- crud-angular/src/app/courses/service/courses.service.ts
+++ crud-angular/src/app/courses/service/courses.service.ts
@@ -15,4 +15,8 @@ export class CoursesService {
 	list() {
 		return this.httpClient.get<Course[]>(this.API).pipe(first(), tap(courses => console.log(courses)));
 	}
+
+	save(record: Course){
+		return this.httpClient.post<Course>(this.API, record).pipe(first())
+	}
 }
