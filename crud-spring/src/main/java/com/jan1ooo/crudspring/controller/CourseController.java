5c3d7d433bb6b9fbe927790d693935103ac8f87f
Jânio Filho <filhojanio67@gmail.com>
2023-06-06 19:44:10 -0300

    Correção de imports

--- crud-spring/src/main/java/com/jan1ooo/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/jan1ooo/crudspring/controller/CourseController.java
@@ -4,14 +4,12 @@ import com.jan1ooo.crudspring.model.Course;
 import com.jan1ooo.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 
-import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/courses")
--- crud-spring/src/main/java/com/jan1ooo/crudspring/repository/CourseRepository.java
+++ crud-spring/src/main/java/com/jan1ooo/crudspring/repository/CourseRepository.java
@@ -2,9 +2,7 @@ package com.jan1ooo.crudspring.repository;
 
 import com.jan1ooo.crudspring.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
