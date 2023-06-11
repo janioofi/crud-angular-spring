196df3263dfb4775bf3e81a9cd689cccbfce55a3
jan1ooo <filhojanio67@gmail.com>
2023-06-11 14:03:41 -0300

    Padrão DTO (Data Transfer Object)

---
+++ crud-spring/src/main/java/com/jan1ooo/crudspring/dto/CourseDTO.java
@@ -0,0 +1,13 @@
+package com.jan1ooo.crudspring.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.Length;
+
+public record CourseDTO(
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 3, max = 100) String name,
+        @NotNull @Pattern(regexp = "Front-end|Back-end") String category,
+        @NotNull @Min(1) @Max(500) Integer hours) {
+
+}
